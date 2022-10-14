93894c0ff8537e22b820290260c294f29e08cae1
Agustin Cadamuro <acadamuro2010@hotmail.com>
2022-10-13 22:59:13 -0300

    first commit

---
+++ assets/js/productos.js
@@ -0,0 +1,26 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Hamburguesa",
+        precio: 900,
+        img: "https://images.aws.nestle.recipes/resized/f10d69e7fede5cd6200a8ddd41b3cb68_hamburguesa-parrillera_708_600.jpg",
+    },
+    {
+        id: 2,
+        nombre: "Pizza",
+        precio: 1300,
+        img: "https://fotos.perfil.com/2022/02/09/trim/1040/780/pizza-1310709.jpg",
+    },
+    {
+        id: 3,
+        nombre: "Lomo",
+        precio: 1000,
+        img: "https://www.circuitogastronomico.com/wp-content/uploads/2022/04/pepecito-800x600.jpg",
+    },
+    {
+        id: 4,
+        nombre: "Empanadas",
+        precio: 950,
+        img: "https://assets.unileversolutions.com/recipes-v2/209726.jpg",
+    },
+];
